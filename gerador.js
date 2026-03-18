2ab92f82d26ae1d1aaf158259b3ed2e1a194619e
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-03-18 11:57:01 -0300

    Add files via upload

---
+++ gerador.js
@@ -0,0 +1,22 @@
+const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+quantidade=0
+caracteres = 5
+numero = Math.floor(Math.random()*62)
+
+for(i=0;i<=caracteres.length;i++){
+    
+}
+
+for(i=0;i<letras.length;i++){
+    quantidade++
+}
+
+console.log(quantidade)
+console.log(numero)
+
+console.log(senha)
+
+numero = 0
+
+
